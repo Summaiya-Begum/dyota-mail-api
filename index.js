2ad1ae5791a628a59265559a8b60319172089e94
Summaiya <gzlfathima786@gmail.com>
2023-05-21 16:25:00 +0530

    Fix: cors issuse

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const { connection } = require("./db");
 const mailRoutes = require("./route/mail.route");
  require('dotenv').config()
 const app = express();
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
